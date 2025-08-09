fd32bbc9663561ac90f4201720a263b2d6e30fdd
Rubric <ethankigotho@gmail.com>
2025-08-08 21:45:41 -0500

    ch15 Complete

--- smart_pointers/src/main.rs
+++ smart_pointers/src/main.rs
@@ -29,12 +29,16 @@ impl Drop for CustomSmartPointer {
     }
 }
 
-// Ch 15.1 & 15.4
+// Ch 15.1 & 15.4 & 15.5
 use std::rc::Rc;
+use std::cell::RefCell;
 use crate::List::{Cons, Nil};
 
-enum List<T> {
-    Cons(T, Rc<List<T>>), // Use Pointer for Indirection
+
+#[derive(Debug)]
+#[allow(dead_code)]
+enum List {
+    Cons(Rc<RefCell<i32>>, Rc<List>), // Pointer for Indirection
     Nil,
 }
 
@@ -49,14 +53,16 @@ fn main() {
     drop(y);
 
 
-    let a = Rc::new(Cons(5, Rc::new(Cons(10, Rc::new(Nil)))));
-    println!("count after creating a = {}", Rc::strong_count(&a));
-    let b = Cons(3, Rc::clone(&a));
-    println!("count after creating b = {}", Rc::strong_count(&a));
-    {
-        let c = Cons(4, Rc::clone(&a));
-        println!("count after creating c = {}", Rc::strong_count(&a));
-    }
-    println!("count after c goes out of scope = {}", Rc::strong_count(&a));
+    let value = Rc::new(RefCell::new(5));
 
+    let a = Rc::new(Cons(Rc::clone(&value), Rc::new(Nil)));
+
+    let b = Cons(Rc::new(RefCell::new(3)), Rc::clone(&a));
+    let c = Cons(Rc::new(RefCell::new(4)), Rc::clone(&a));
+
+    *value.borrow_mut() += 10;
+
+    println!("a after = {a:?}");
+    println!("b after = {b:?}");
+    println!("c after = {c:?}");
 }
